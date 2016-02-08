407b9e0d58445b2c4755b9ae5aad962c0b5f77a7
Wojtek Mach <wojtek@wojtekmach.pl>
2016-02-09 00:19:18 +0100

    Extract SearchPath module

--- lib/github_ecto.ex
+++ lib/github_ecto.ex
@@ -9,6 +9,72 @@ defmodule GitHub.Ecto do
     end
   end
 
+  defmodule SearchPath do
+    def build(query) do
+      {from, _} = query.from
+
+      str =
+        [where(query), order_by(query), limit_offset(query)]
+        |> Enum.filter(&(&1 != ""))
+        |> Enum.join("&")
+
+      "/search/#{from}?#{str}"
+    end
+
+    defp where(%Ecto.Query{wheres: wheres}) do
+      "q=" <> Enum.map_join(wheres, "", fn where ->
+        %Ecto.Query.QueryExpr{expr: expr} = where
+        parse_where(expr)
+      end)
+    end
+
+    defp parse_where({:and, [], [left, right]}) do
+      "#{parse_where(left)}+#{parse_where(right)}"
+    end
+    defp parse_where({:==, [], [_, %Ecto.Query.Tagged{tag: nil, type: {0, field}, value: value}]}) do
+      "#{field}:#{value}"
+    end
+    defp parse_where({:==, [], [{{:., [], [{:&, [], [0]}, field]}, [ecto_type: :any], []}, value]}) do
+      "#{field}:#{value}"
+    end
+
+    defp order_by(%Ecto.Query{order_bys: [order_by]}) do
+      %Ecto.Query.QueryExpr{expr: expr} = order_by
+      [{order, {{:., [], [{:&, [], [0]}, sort]}, _, []}}] = expr
+
+      "sort=#{sort}&order=#{order}"
+    end
+    defp order_by(%Ecto.Query{order_bys: []}), do: ""
+    defp order_by(_), do: raise ArgumentError, "GitHub API can only order by one field"
+
+    defp limit_offset(%Ecto.Query{limit: limit, offset: offset}) do
+      limit = if limit do
+        %Ecto.Query.QueryExpr{expr: expr} = limit
+        expr
+      end
+
+      offset = if offset do
+        %Ecto.Query.QueryExpr{expr: expr} = offset
+        expr
+      end
+
+      case {limit, offset} do
+        {nil, nil} ->
+          ""
+
+        {limit, nil} ->
+          "per_page=#{limit}"
+
+        {nil, offset} ->
+          "page=2&per_page=#{offset}"
+
+        {limit, offset} ->
+          page = (offset / limit) + 1 |> round
+          "page=#{page}&per_page=#{limit}"
+      end
+    end
+  end
+
   ## Boilerplate
 
   @behaviour Ecto.Adapter
@@ -33,74 +99,10 @@ defmodule GitHub.Ecto do
 
   def execute(_repo, _meta, prepared, [] = _params, _preprocess, [] = _opts) do
     {_, query} = prepared
-    path = to_search_path(query)
+    path = SearchPath.build(query)
     items = Client.search(path) |> Enum.map(fn item -> [item] end)
 
     {0, items}
-  end
-
-  def to_search_path(query) do
-    {from, _} = query.from
-
-    str =
-      [where(query), order_by(query), limit_offset(query)]
-      |> Enum.filter(&(&1 != ""))
-      |> Enum.join("&")
-
-    "/search/#{from}?#{str}"
-  end
-
-  defp where(%Ecto.Query{wheres: wheres}) do
-    "q=" <> Enum.map_join(wheres, "", fn where ->
-      %Ecto.Query.QueryExpr{expr: expr} = where
-      parse_where(expr)
-    end)
-  end
-
-  defp parse_where({:and, [], [left, right]}) do
-    "#{parse_where(left)}+#{parse_where(right)}"
-  end
-  defp parse_where({:==, [], [_, %Ecto.Query.Tagged{tag: nil, type: {0, field}, value: value}]}) do
-    "#{field}:#{value}"
-  end
-  defp parse_where({:==, [], [{{:., [], [{:&, [], [0]}, field]}, [ecto_type: :any], []}, value]}) do
-    "#{field}:#{value}"
-  end
-
-  defp order_by(%Ecto.Query{order_bys: [order_by]}) do
-    %Ecto.Query.QueryExpr{expr: expr} = order_by
-    [{order, {{:., [], [{:&, [], [0]}, sort]}, _, []}}] = expr
-
-    "sort=#{sort}&order=#{order}"
-  end
-  defp order_by(%Ecto.Query{order_bys: []}), do: ""
-  defp order_by(_), do: raise ArgumentError, "GitHub API can only order by one field"
-
-  defp limit_offset(%Ecto.Query{limit: limit, offset: offset}) do
-    limit = if limit do
-      %Ecto.Query.QueryExpr{expr: expr} = limit
-      expr
-    end
-
-    offset = if offset do
-      %Ecto.Query.QueryExpr{expr: expr} = offset
-      expr
-    end
-
-    case {limit, offset} do
-      {nil, nil} ->
-        ""
-
-      {limit, nil} ->
-        "per_page=#{limit}"
-
-      {nil, offset} ->
-        "page=2&per_page=#{offset}"
-
-      {limit, offset} ->
-        page = (offset / limit) + 1 |> round
-        "page=#{page}&per_page=#{limit}"
-    end
   end
 
   ## Writes
--- test/github_ecto_test.exs
+++ test/github_ecto_test.exs
@@ -9,50 +9,56 @@ defmodule GitHub.EctoTest do
     :ok
   end
 
-  test "to_search_path: search different entities" do
-    q = from r in "repositories", where: r.user == "elixir-lang"
-    assert GitHub.Ecto.to_search_path(q) == "/search/repositories?q=user:elixir-lang"
-
-    q = from i in "issues", where: i.repo == "elixir-lang/ecto"
-    assert GitHub.Ecto.to_search_path(q) == "/search/issues?q=repo:elixir-lang/ecto"
-  end
-
-  test "to_search_path: multiple fields" do
-    q = from i in "issues", where: i.repo == "elixir-lang/ecto" and i.state == "closed"
-    assert GitHub.Ecto.to_search_path(q) == "/search/issues?q=repo:elixir-lang/ecto+state:closed"
-  end
-
-  test "to_search_path: order_by" do
-    q = from i in "issues", where: i.repo == "elixir-lang/ecto", order_by: i.created
-    assert GitHub.Ecto.to_search_path(q) == "/search/issues?q=repo:elixir-lang/ecto&sort=created&order=asc"
-
-    q = from i in "issues", where: i.repo == "elixir-lang/ecto", order_by: [desc: i.created]
-    assert GitHub.Ecto.to_search_path(q) == "/search/issues?q=repo:elixir-lang/ecto&sort=created&order=desc"
-  end
-
-  test "to_search_path: limit" do
-    q = from i in "issues", where: i.repo == "elixir-lang/ecto", limit: 10
-    assert GitHub.Ecto.to_search_path(q) == "/search/issues?q=repo:elixir-lang/ecto&per_page=10"
-  end
-
-  test "to_search_path: offset" do
-    q = from i in "issues", where: i.repo == "elixir-lang/ecto", offset: 10
-    assert GitHub.Ecto.to_search_path(q) == "/search/issues?q=repo:elixir-lang/ecto&page=2&per_page=10"
-  end
-
-  test "to_search_path: limit and offset" do
-    q = from i in "issues", where: i.repo == "elixir-lang/ecto", limit: 5, offset: 10
-    assert GitHub.Ecto.to_search_path(q) == "/search/issues?q=repo:elixir-lang/ecto&page=3&per_page=5"
-  end
-
   test "search issues" do
     use_cassette("search issues") do
       q = from i in "issues", where: i.repo == "elixir-lang/ecto" and i.state == "closed", order_by: [asc: i.created]
-      assert GitHub.Ecto.to_search_path(q) == "/search/issues?q=repo:elixir-lang/ecto+state:closed&sort=created&order=asc"
+      assert GitHub.Ecto.SearchPath.build(q) == "/search/issues?q=repo:elixir-lang/ecto+state:closed&sort=created&order=asc"
 
       issues = TestRepo.all(q)
       assert length(issues) == 30
       assert %{"title" => "Minor cleanup on smart escape", "state" => "closed", "url" => "https://api.github.com/repos/elixir-lang/ecto/issues/1"} = hd(issues)
     end
+  end
+end
+
+defmodule GitHub.Ecto.SearchPathTest do
+  use ExUnit.Case, async: true
+  import Ecto.Query, only: [from: 2]
+  import GitHub.Ecto.SearchPath, only: [build: 1]
+
+  test "from" do
+    q = from r in "repositories", where: r.user == "elixir-lang"
+    assert build(q) == "/search/repositories?q=user:elixir-lang"
+
+    q = from i in "issues", where: i.repo == "elixir-lang/ecto"
+    assert build(q) == "/search/issues?q=repo:elixir-lang/ecto"
+  end
+
+  test "multiple fields" do
+    q = from i in "issues", where: i.repo == "elixir-lang/ecto" and i.state == "closed"
+    assert build(q) == "/search/issues?q=repo:elixir-lang/ecto+state:closed"
+  end
+
+  test "order_by" do
+    q = from i in "issues", where: i.repo == "elixir-lang/ecto", order_by: i.created
+    assert build(q) == "/search/issues?q=repo:elixir-lang/ecto&sort=created&order=asc"
+
+    q = from i in "issues", where: i.repo == "elixir-lang/ecto", order_by: [desc: i.created]
+    assert build(q) == "/search/issues?q=repo:elixir-lang/ecto&sort=created&order=desc"
+  end
+
+  test "limit" do
+    q = from i in "issues", where: i.repo == "elixir-lang/ecto", limit: 10
+    assert build(q) == "/search/issues?q=repo:elixir-lang/ecto&per_page=10"
+  end
+
+  test "offset" do
+    q = from i in "issues", where: i.repo == "elixir-lang/ecto", offset: 10
+    assert build(q) == "/search/issues?q=repo:elixir-lang/ecto&page=2&per_page=10"
+  end
+
+  test "limit and offset" do
+    q = from i in "issues", where: i.repo == "elixir-lang/ecto", limit: 5, offset: 10
+    assert build(q) == "/search/issues?q=repo:elixir-lang/ecto&page=3&per_page=5"
   end
 end
