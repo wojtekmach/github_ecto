c5fa1d7911307b6637871610ae2b5f9316530329
Wojtek Mach <wojtek@wojtekmach.pl>
2017-03-29 17:21:06 +0200

    Remove warnings

--- mix.exs
+++ mix.exs
@@ -2,32 +2,22 @@ defmodule GitHub.Ecto.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :github_ecto,
-     version: "0.0.1",
-     description: "Ecto adapter for GitHub API",
-     package: package,
-     elixir: "~> 1.0",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :github_ecto,
+      version: "0.0.1",
+      description: "Ecto adapter for GitHub API",
+      package: package(),
+      elixir: "~> 1.0",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :ecto, :httpoison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:ecto, "~> 2.0.0-beta"},
